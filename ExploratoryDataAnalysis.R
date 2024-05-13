3f9ec07860e6403b19272b9281d390116eabca0f
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-13 17:29:38 +0300

    Measures of frequency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -23,3 +23,35 @@ head(housing_data)
 
 # View the dataset in a separate viewer window
 View(housing_data)
+
+# Measures of frequency for categorical variables
+table_mainroad <- table(housing_data$mainroad)
+table_guestroom <- table(housing_data$guestroom)
+table_basement <- table(housing_data$basement)
+table_hotwater <- table(housing_data$hotwaterheating)
+table_airconditioning <- table(housing_data$airconditioning)
+table_prefarea <- table(housing_data$prefarea)
+table_furnishingstatus <- table(housing_data$furnishingstatus)
+
+# Display measures of frequency
+print("Frequency of Mainroad:")
+print(table_mainroad)
+
+print("Frequency of Guestroom:")
+print(table_guestroom)
+
+print("Frequency of Basement:")
+print(table_basement)
+
+print("Frequency of Hot Water Heating:")
+print(table_hotwater)
+
+print("Frequency of Air Conditioning:")
+print(table_airconditioning)
+
+print("Frequency of Preferred Area:")
+print(table_prefarea)
+
+print("Frequency of Furnishing Status:")
+print(table_furnishingstatus)
+
